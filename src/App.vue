07343ccd992dd0e8eb9e72a5e1789a984e119010
Imad Saddik <simad3647@gmail.com>
2025-07-19 18:28:35 +0100

    Changed the default font to Inter

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
