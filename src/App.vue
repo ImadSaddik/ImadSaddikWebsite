ce79510268871f2818b3aa5cc4c22f59e844849f
Imad Saddik <simad3647@gmail.com>
2025-07-19 18:38:43 +0100

    Added CSS variables for font sizes

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,19 @@
 @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
 
 :root {
+  /* Colors */
   --color-background: #03071e;
   --color-button-primary: #f97316;
   --color-button-secondary: #22d3ee;
   --color-text-primary: #e5e7eb;
   --color-text-secondary: #9ca3af;
   --color-text-disabled: #4b5563;
+
+  /* Font sizes */
+  --font-size-page-title: 2.1875rem; /* 35px */
+  --font-size-section-title: 1.75rem; /* 28px */
+  --font-size-sub-title: 1.4375rem; /* 23px */
+  --font-size-paragraph: 1.125rem; /* 18px */
 
   background: var(--color-background);
 }
