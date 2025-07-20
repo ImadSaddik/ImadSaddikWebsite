4789a6b26584f86fe0099bfd8b72b5baedffc4c4
Imad Saddik <simad3647@gmail.com>
2025-07-20 15:51:15 +0100

    Added few CSS variables and customized the text selection

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,15 @@ export default {
   /* Colors */
   --color-background: #03071e;
   --color-button-primary: #f97316;
+  --color-button-primary-hover: #cb5f12;
   --color-button-secondary: #22d3ee;
+  --color-button-secondary-hover: #167595;
   --color-text-primary: #e5e7eb;
   --color-text-secondary: #9ca3af;
   --color-text-disabled: #4b5563;
 
   /* Font sizes */
+  --font-size-page-huge-title: 3.125rem; /* 50px */
   --font-size-page-title: 2.1875rem; /* 35px */
   --font-size-section-title: 1.75rem; /* 28px */
   --font-size-sub-title: 1.4375rem; /* 23px */
@@ -38,6 +41,12 @@ export default {
   --gap-md: 1.5rem; /* 24px */
   --gap-lg: 2rem; /* 32px */
   --gap-xl: 3rem; /* 48px */
+  --gap-xxl: 4rem; /* 64px */
+}
+
+::selection {
+  background: var(--color-button-primary);
+  color: var(--color-background);
 }
 
 #app {
