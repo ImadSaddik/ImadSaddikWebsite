5bde92a9cc3ae67c43d5ba4dd2c659d7f9e4a527
Imad Saddik <simad3647@gmail.com>
2025-07-29 08:08:03 +0100

    Made the CardGroup responsive

--- src/components/CardGroup.vue
+++ src/components/CardGroup.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="cards-group-container">
-    <h2 class="title">{{ title }}</h2>
+    <h2 class="cards-group-title">{{ title }}</h2>
 
     <div class="cards-group">
       <Card
@@ -13,7 +13,7 @@
       />
     </div>
 
-    <button class="custom-button primary-button">{{ buttonText }}</button>
+    <button class="cards-group-button primary-button">{{ buttonText }}</button>
   </section>
 </template>
 
@@ -57,11 +57,53 @@ export default {
   width: 75%;
 }
 
+.cards-group-title {
+  font-size: var(--font-size-big);
+  margin-top: 0;
+  margin-bottom: var(--gap-md);
+}
+
 .cards-group {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   width: 100%;
   gap: var(--gap-md);
   margin-bottom: var(--gap-xl);
+}
+
+@media screen and (max-width: 2000px) {
+  .cards-group-container {
+    width: 80%;
+  }
+}
+
+@media screen and (max-width: 1500px) {
+  .cards-group-container {
+    width: 90%;
+    margin-top: var(--gap-between-sections-medium);
+  }
+}
+
+@media screen and (max-width: 1100px) {
+  .cards-group-container {
+    width: 100%;
+    margin-top: var(--gap-between-sections-small);
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .cards-group-title {
+    font-size: var(--font-size-big-medium);
+  }
+
+  .cards-group {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .cards-group {
+    grid-template-columns: repeat(1, 1fr);
+  }
 }
 </style>
