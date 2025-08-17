a178c8ba9edb735ce0684797b3da47404f3fd29c
Imad Saddik <simad3647@gmail.com>
2025-08-17 11:06:42 +0100

    Added a component that displays inline code

---
+++ src/components/InlineCode.vue
@@ -0,0 +1,31 @@
+<template>
+  <code>{{ text }}</code>
+</template>
+
+<script>
+export default {
+  name: "InlineCode",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+code {
+  background-color: var(--color-tag-background);
+  color: var(--color-secondary);
+  padding: 2px 6px;
+  font-size: var(--font-size-small);
+  font-weight: 600;
+}
+
+@media screen and (max-width: 576px) {
+  code {
+    font-weight: 400;
+  }
+}
+</style>
