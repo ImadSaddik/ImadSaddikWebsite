8a873d30b19d64f465881b96b959357d24a04121
Imad Saddik <simad3647@gmail.com>
2025-08-17 11:06:09 +0100

    Added a component that creates an image with a caption

---
+++ src/components/ImageWithCaption.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <figure>
+      <img :src="imageSrc" :alt="imageAlt" @click="openImageModal" />
+      <figcaption>{{ imageCaption }}</figcaption>
+    </figure>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ImageWithCaption",
+  emits: ["openImageModal"],
+  props: {
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+    imageAlt: {
+      type: String,
+      required: true,
+    },
+    imageCaption: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    openImageModal(event) {
+      this.$emit("openImageModal", event);
+    },
+  },
+};
+</script>
+
+<style scoped>
+figure {
+  margin: 0;
+}
+
+img {
+  width: 100%;
+  height: auto;
+  cursor: zoom-in;
+}
+
+figcaption {
+  text-align: center;
+  color: var(--color-text-secondary);
+  font-size: var(--font-size-small);
+  margin-top: var(--gap-xs);
+}
+</style>
