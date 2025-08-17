b98954cbf5dc01e30ede745a4584dbdc3e09a259
Imad Saddik <simad3647@gmail.com>
2025-08-17 11:05:49 +0100

    Added a component that allows to click on an image to enlarge it

---
+++ src/components/ImageEnlarger.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="isVisible" class="image-modal-overlay" @click="closeImageModal">
+    <img :src="enlargedImageSrc" class="image-modal-content" alt="Enlarged view of the image." />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageEnlarger",
+  emits: ["closeImageModal"],
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false,
+    },
+    enlargedImageSrc: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    closeImageModal() {
+      this.$emit("closeImageModal");
+    },
+    handleEscape(event) {
+      if (event.key === "Escape") {
+        this.closeImageModal();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.image-modal-overlay {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-background-overlay);
+  z-index: 1000;
+  cursor: zoom-out;
+}
+
+.image-modal-content {
+  width: 90vw;
+  height: 90vh;
+  object-fit: contain;
+}
+</style>
