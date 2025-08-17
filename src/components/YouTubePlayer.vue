25e8b2c867331d40ebdbc6d7238729fa8df2bf2f
Imad Saddik <simad3647@gmail.com>
2025-08-17 11:04:22 +0100

    Added a component that embeds YouTube videos

---
+++ src/components/YouTubePlayer.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="youtube-player-container">
+    <iframe
+      :src="videoUrl"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "YouTubePlayer",
+  props: {
+    videoUrl: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.youtube-player-container {
+  position: relative;
+  width: 100%;
+  aspect-ratio: 16 / 9;
+  margin-bottom: var(--gap-md);
+}
+
+iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
