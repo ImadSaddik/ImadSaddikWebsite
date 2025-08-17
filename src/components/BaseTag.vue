982d265147fdcc83088f3cfef3ebfab79d41ffaf
Imad Saddik <simad3647@gmail.com>
2025-08-17 11:05:13 +0100

    Added a component to display tags for articles

---
+++ src/components/BaseTag.vue
@@ -0,0 +1,41 @@
+<template>
+  <span class="base-tag">{{ name }}</span>
+</template>
+
+<script>
+export default {
+  name: "BaseTag",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.base-tag {
+  background-color: var(--color-tag-background);
+  color: var(--color-secondary);
+  padding: var(--gap-xs) var(--gap-sm);
+  font-size: var(--font-size-small);
+  font-weight: 500;
+  border: 1px solid var(--color-tag-border);
+  transition: all 0.2s ease;
+  cursor: pointer;
+}
+
+.base-tag:hover {
+  background-color: var(--color-tag-background-hover);
+  border-color: var(--color-tag-border-hover);
+}
+
+@media screen and (max-width: 576px) {
+  .base-tag {
+    font-size: var(--font-size-small);
+    padding: var(--gap-xs);
+    font-weight: 400;
+  }
+}
+</style>
