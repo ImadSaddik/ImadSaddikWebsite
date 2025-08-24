1ec157bd68ea31a14fca4305023ee35355ce3295
Imad Saddik <simad3647@gmail.com>
2025-08-24 11:09:36 +0100

    Created the ArticlesHub component that will be used by blogs, courses, and astronomy tutorials

---
+++ frontend/src/components/ArticlesHub.vue
@@ -0,0 +1,256 @@
+<template>
+  <section class="articles-hub-container">
+    <div>
+      <h1 class="articles-hub-title">{{ articleType }} hub</h1>
+      <SearchBar :placeHolder="searchPlaceholder" @search-request="handleSearchRequest" />
+    </div>
+
+    <div class="articles-hub-columns">
+      <div class="articles-hub-filters-column">
+        <div class="articles-hub-filters-sorting">
+          <h2 @click="toggleSortingExpanded" class="collapsible-header">
+            Sort
+            <i class="fa-solid" :class="sortingExpanded ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
+          </h2>
+          <div v-if="sortingExpanded" class="collapsible-content">
+            <DropDownMenu clearable v-model="sortOption" :placeholder="sortPlaceholder" :options="sortOptions" />
+          </div>
+        </div>
+
+        <div class="articles-hub-filters-year">
+          <h2 @click="toggleYearExpanded" class="collapsible-header">
+            Year
+            <i class="fa-solid" :class="yearExpanded ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
+          </h2>
+          <div v-if="yearExpanded" class="collapsible-content">
+            <CheckboxGroup v-model="selectedYears" :options="yearOptions" />
+          </div>
+        </div>
+
+        <div class="articles-hub-filters-tags">
+          <h2 @click="toggleTagsExpanded" class="collapsible-header">
+            Tags
+            <i class="fa-solid" :class="tagsExpanded ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
+          </h2>
+          <div v-if="tagsExpanded" class="collapsible-content">
+            <CheckboxGroup v-model="selectedTags" :options="tagOptions" />
+          </div>
+        </div>
+      </div>
+
+      <div class="articles-hub-search-result">
+        <div class="cards-group">
+          <BaseCard
+            v-for="(card, idx) in cardData"
+            :key="idx"
+            :image-src="card.imageSrc"
+            :alt-text="card.altText"
+            :title="card.title"
+            :sub-title="card.subTitle"
+            :article-type="card.articleType"
+            :article-id="card.articleId"
+          />
+        </div>
+
+        <button class="load-more-button primary-button">Load more</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// Components
+import DropDownMenu from "@/components/DropDownMenu.vue";
+import CheckboxGroup from "@/components/CheckboxGroup.vue";
+import BaseCard from "@/components/BaseCard.vue";
+import SearchBar from "@/components/SearchBar.vue";
+
+export default {
+  name: "ArticlesHub",
+  props: {
+    articleType: {
+      type: String,
+      required: true,
+    },
+    searchPlaceholder: {
+      type: String,
+      required: true,
+      default: "What article are you curious about today?",
+    },
+    sortPlaceholder: {
+      type: String,
+      required: true,
+      default: "Show me articles sorted by...",
+    },
+    cardData: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  components: {
+    DropDownMenu,
+    CheckboxGroup,
+    BaseCard,
+    SearchBar,
+  },
+  data() {
+    return {
+      sortingExpanded: true,
+      yearExpanded: true,
+      tagsExpanded: true,
+
+      sortOption: null,
+      sortOptions: [
+        { value: "date", label: "Date" },
+        { value: "relevance", label: "Relevance" },
+      ],
+
+      selectedYears: [],
+      yearOptions: [
+        { value: 2025, label: "2025" },
+        { value: 2024, label: "2024" },
+        { value: 2023, label: "2023" },
+      ],
+
+      selectedTags: [],
+      tagOptions: [
+        { value: "vue", label: "Vue.js" },
+        { value: "javascript", label: "JavaScript" },
+        { value: "css", label: "CSS" },
+      ],
+    };
+  },
+  methods: {
+    toggleSortingExpanded() {
+      this.sortingExpanded = !this.sortingExpanded;
+    },
+    toggleYearExpanded() {
+      this.yearExpanded = !this.yearExpanded;
+    },
+    toggleTagsExpanded() {
+      this.tagsExpanded = !this.tagsExpanded;
+    },
+    handleSearchRequest() {},
+  },
+};
+</script>
+
+<style scoped>
+.articles-hub-container {
+  padding: var(--gap-xl);
+  margin-top: var(--gap-xxl);
+}
+
+.articles-hub-title {
+  font-size: var(--font-size-big);
+  margin-top: 0;
+  margin-bottom: var(--gap-md);
+}
+
+.articles-hub-columns {
+  display: flex;
+  flex-direction: row;
+  gap: var(--gap-xl);
+  margin-top: var(--gap-xxl);
+}
+
+.articles-hub-filters-column {
+  flex: 1;
+}
+
+.articles-hub-filters-column h2 {
+  margin: 0;
+  font-size: var(--font-size-medium);
+}
+
+.articles-hub-filters-year {
+  margin-top: var(--gap-lg);
+}
+
+.articles-hub-filters-tags {
+  margin-top: var(--gap-lg);
+}
+
+.collapsible-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
+  transition: color 0.2s ease;
+}
+
+.collapsible-header:hover {
+  color: var(--color-primary);
+}
+
+.collapsible-header i {
+  color: var(--color-text-primary);
+  transition: color 0.2s ease;
+}
+
+.collapsible-header:hover i {
+  color: var(--color-primary);
+}
+
+.collapsible-content {
+  margin-top: var(--gap-sm);
+}
+
+.articles-hub-search-result {
+  flex: 4;
+}
+
+.cards-group {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: var(--gap-md);
+  width: 100%;
+}
+
+.load-more-button {
+  padding: var(--gap-sm) var(--gap-xs);
+  font-size: var(--font-size-small);
+  font-weight: 600;
+  width: 200px;
+  margin-top: var(--gap-xl);
+}
+
+@media screen and (max-width: 1100px) {
+  .articles-hub-container {
+    padding: var(--gap-lg);
+    margin-top: var(--gap-md);
+  }
+
+  .articles-hub-title {
+    font-size: var(--font-size-big-medium);
+    margin-bottom: var(--gap-md);
+  }
+
+  .articles-hub-search-result {
+    flex: 2;
+  }
+
+  .cards-group {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .articles-hub-container {
+    padding: var(--gap-md);
+    margin-top: var(--gap-sm);
+  }
+
+  .articles-hub-columns {
+    flex-direction: column;
+    gap: var(--gap-lg);
+    margin-top: var(--gap-lg);
+  }
+
+  .articles-hub-filters-column h2 {
+    font-size: var(--font-size-big-small);
+  }
+}
+</style>
--- frontend/src/views/BlogsHub.vue
+++ frontend/src/views/BlogsHub.vue
@@ -1,74 +1,15 @@
 <template>
-  <section class="blog-hub-container">
-    <div>
-      <h1 class="blog-hub-title">Blogs hub</h1>
-      <SearchBar placeHolder="What article are you curious about today?" @search-request="handleSearchRequest" />
-    </div>
-
-    <div class="blog-hub-columns">
-      <div class="blog-hub-filters-column">
-        <div class="blog-hub-filters-sorting">
-          <h2 @click="toggleSortingExpanded" class="collapsible-header">
-            Sort
-            <i class="fa-solid" :class="sortingExpanded ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
-          </h2>
-          <div v-if="sortingExpanded" class="collapsible-content">
-            <DropDownMenu
-              clearable
-              v-model="sortOption"
-              placeholder="Show me articles sorted by..."
-              :options="sortOptions"
-            />
-          </div>
-        </div>
-
-        <div class="blog-hub-filters-year">
-          <h2 @click="toggleYearExpanded" class="collapsible-header">
-            Year
-            <i class="fa-solid" :class="yearExpanded ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
-          </h2>
-          <div v-if="yearExpanded" class="collapsible-content">
-            <CheckboxGroup v-model="selectedYears" :options="yearOptions" />
-          </div>
-        </div>
-
-        <div class="blog-hub-filters-tags">
-          <h2 @click="toggleTagsExpanded" class="collapsible-header">
-            Tags
-            <i class="fa-solid" :class="tagsExpanded ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
-          </h2>
-          <div v-if="tagsExpanded" class="collapsible-content">
-            <CheckboxGroup v-model="selectedTags" :options="tagOptions" />
-          </div>
-        </div>
-      </div>
-
-      <div class="blog-hub-search-result">
-        <div class="cards-group">
-          <BaseCard
-            v-for="(card, idx) in cardData"
-            :key="idx"
-            :image-src="card.imageSrc"
-            :alt-text="card.altText"
-            :title="card.title"
-            :sub-title="card.subTitle"
-            :article-type="card.articleType"
-            :article-id="card.articleId"
-          />
-        </div>
-
-        <button class="load-more-button primary-button">Load more</button>
-      </div>
-    </div>
-  </section>
+  <ArticlesHub
+    article-type="Blogs"
+    search-placeholder="What blogs are you curious about today?"
+    sort-placeholder="Show me blogs sorted by..."
+    :card-data="cardData"
+  />
 </template>
 
 <script>
 // Components
-import DropDownMenu from "@/components/DropDownMenu.vue";
-import CheckboxGroup from "@/components/CheckboxGroup.vue";
-import BaseCard from "@/components/BaseCard.vue";
-import SearchBar from "@/components/SearchBar.vue";
+import ArticlesHub from "@/components/ArticlesHub.vue";
 
 // Images
 import blogcoverImage1 from "@/blogs/ElasticsearchPreFilteringWithKnnSearch/coverImage.svg";
@@ -78,37 +19,11 @@ import blogcoverImage3 from "@/blogs/ElasticsearchChangeHeapSize/coverImage.svg"
 export default {
   name: "BlogHub",
   components: {
-    DropDownMenu,
-    CheckboxGroup,
-    BaseCard,
-    SearchBar,
+    ArticlesHub,
   },
   data() {
     return {
-      sortingExpanded: true,
-      yearExpanded: true,
-      tagsExpanded: true,
-
-      sortOption: null,
-      sortOptions: [
-        { value: "date", label: "Date" },
-        { value: "relevance", label: "Relevance" },
-      ],
-
-      selectedYears: [],
-      yearOptions: [
-        { value: 2025, label: "2025" },
-        { value: 2024, label: "2024" },
-        { value: 2023, label: "2023" },
-      ],
-
-      selectedTags: [],
-      tagOptions: [
-        { value: "vue", label: "Vue.js" },
-        { value: "javascript", label: "JavaScript" },
-        { value: "css", label: "CSS" },
-      ],
-
+      articleType: "blog-post",
       cardData: [
         {
           imageSrc: blogcoverImage1,
@@ -137,136 +52,8 @@ export default {
       ],
     };
   },
-  methods: {
-    toggleSortingExpanded() {
-      this.sortingExpanded = !this.sortingExpanded;
-    },
-    toggleYearExpanded() {
-      this.yearExpanded = !this.yearExpanded;
-    },
-    toggleTagsExpanded() {
-      this.tagsExpanded = !this.tagsExpanded;
-    },
-    handleSearchRequest() {},
-  },
+  methods: {},
 };
 </script>
 
-<style scoped>
-.blog-hub-container {
-  padding: var(--gap-xl);
-  margin-top: var(--gap-xxl);
-}
-
-.blog-hub-title {
-  font-size: var(--font-size-big);
-  margin-top: 0;
-  margin-bottom: var(--gap-md);
-}
-
-.blog-hub-columns {
-  display: flex;
-  flex-direction: row;
-  gap: var(--gap-xl);
-  margin-top: var(--gap-xxl);
-}
-
-.blog-hub-filters-column {
-  flex: 1;
-}
-
-.blog-hub-filters-column h2 {
-  margin: 0;
-  font-size: var(--font-size-medium);
-}
-
-.blog-hub-filters-year {
-  margin-top: var(--gap-lg);
-}
-
-.blog-hub-filters-tags {
-  margin-top: var(--gap-lg);
-}
-
-.collapsible-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-  user-select: none;
-  transition: color 0.2s ease;
-}
-
-.collapsible-header:hover {
-  color: var(--color-primary);
-}
-
-.collapsible-header i {
-  color: var(--color-text-primary);
-  transition: color 0.2s ease;
-}
-
-.collapsible-header:hover i {
-  color: var(--color-primary);
-}
-
-.collapsible-content {
-  margin-top: var(--gap-sm);
-}
-
-.blog-hub-search-result {
-  flex: 4;
-}
-
-.cards-group {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: var(--gap-md);
-  width: 100%;
-}
-
-.load-more-button {
-  padding: var(--gap-sm) var(--gap-xs);
-  font-size: var(--font-size-small);
-  font-weight: 600;
-  width: 200px;
-  margin-top: var(--gap-xl);
-}
-
-@media screen and (max-width: 1100px) {
-  .blog-hub-container {
-    padding: var(--gap-lg);
-    margin-top: var(--gap-md);
-  }
-
-  .blog-hub-title {
-    font-size: var(--font-size-big-medium);
-    margin-bottom: var(--gap-md);
-  }
-
-  .blog-hub-search-result {
-    flex: 2;
-  }
-
-  .cards-group {
-    grid-template-columns: repeat(1, 1fr);
-  }
-}
-
-@media screen and (max-width: 768px) {
-  .blog-hub-container {
-    padding: var(--gap-md);
-    margin-top: var(--gap-sm);
-  }
-
-  .blog-hub-columns {
-    flex-direction: column;
-    gap: var(--gap-lg);
-    margin-top: var(--gap-lg);
-  }
-
-  .blog-hub-filters-column h2 {
-    font-size: var(--font-size-big-small);
-  }
-}
-</style>
+<style scoped></style>
