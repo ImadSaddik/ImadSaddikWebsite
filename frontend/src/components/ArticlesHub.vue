e748477c653c0b19dc8a458548ea852f89778438
Imad Saddik <simad3647@gmail.com>
2025-08-24 17:01:36 +0100

    Updated the type of the values to string in yearOptions

--- frontend/src/components/ArticlesHub.vue
+++ frontend/src/components/ArticlesHub.vue
@@ -115,9 +115,9 @@ export default {
 
       selectedYears: [],
       yearOptions: [
-        { value: 2025, label: "2025" },
-        { value: 2024, label: "2024" },
-        { value: 2023, label: "2023" },
+        { value: "2025", label: "2025" },
+        { value: "2024", label: "2024" },
+        { value: "2023", label: "2023" },
       ],
 
       selectedTags: [],
