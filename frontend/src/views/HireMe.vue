bbd97ee0216e6f066bc3cd5f6330824a91c348b0
Imad Saddik <simad3647@gmail.com>
2025-09-17 06:54:08 +0100

    Created the header section

--- frontend/src/views/HireMe.vue
+++ frontend/src/views/HireMe.vue
@@ -1,4 +1,16 @@
-<template>hi</template>
+<template>
+  <div class="hire-me-container">
+    <section>
+      <h1>Let's build something great</h1>
+      <p>
+        I'm an engineer, and technical educator. I have experience in <b>web & mobile development</b> and
+        <b>AI & Data Science</b>. I offer a wide range of services to help you achieve your goals. I am looking for new
+        opportunities, either full-time or part-time, to work on exciting projects and collaborate with people on what I
+        find interesting.
+      </p>
+    </section>
+  </div>
+</template>
 
 <script>
 // Constants
@@ -12,7 +24,60 @@ export default {
     document.title = "Hire Imad Saddik";
     this.$emit("page-visited", HIRE_ME_PAGE_VISITED_KEY);
   },
+  data() {
+    return {};
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+p {
+  font-size: var(--font-size-small);
+  color: var(--color-text-secondary);
+  margin: var(--gap-md) 0;
+  line-height: 1.6;
+}
+
+h1 {
+  font-size: var(--font-size-big);
+  margin-top: 0;
+  margin-bottom: var(--gap-md);
+}
+
+h2 {
+  font-size: var(--font-size-big-medium);
+  margin: var(--gap-md) 0;
+}
+
+h3 {
+  font-size: var(--font-size-big-small);
+  margin: var(--gap-sm) 0;
+}
+
+.hire-me-container {
+  width: 50%;
+  padding: var(--gap-xl);
+  margin-top: var(--gap-xxl);
+  box-sizing: border-box;
+}
+
+@media screen and (max-width: 1500px) {
+  .hire-me-container {
+    width: 100%;
+  }
+
+  .hire-me-container h1 {
+    font-size: var(--font-size-big-medium);
+  }
+
+  .hire-me-container h2 {
+    font-size: var(--font-size-medium);
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .hire-me-container {
+    padding: var(--gap-md);
+  }
+}
+</style>
