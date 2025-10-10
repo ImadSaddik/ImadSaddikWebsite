c10d4cee8edc3c818f343563c4fd2e0cbc92a406
Imad Saddik <simad3647@gmail.com>
2025-10-10 12:26:29 +0100

    Used the custom logger, middleware, and exception handlers

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,25 @@
 from api import article, search
+from exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+    unhandled_exception_handler,
+)
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from middleware import log_request_middleware
+from starlette.exceptions import HTTPException
 
 app = FastAPI(title="API for ImadSaddik.com", version="1.0.0")
 
+app.add_exception_handler(RequestValidationError, request_validation_exception_handler)
+app.add_exception_handler(HTTPException, http_exception_handler)
+app.add_exception_handler(Exception, unhandled_exception_handler)
+
+app.middleware("http")(log_request_middleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:8080", "http://192.168.1.15:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
