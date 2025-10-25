234fb125e288dc1d0d4805aad440766202c6bb18
Imad Saddik <simad3647@gmail.com>
2025-10-25 08:52:35 +0100

    Created a new article in the astronomy category : PythonStarTrails

---
+++ frontend/src/components/SuperscriptText.vue
@@ -0,0 +1,25 @@
+<template>
+  <sup>{{ text }}</sup>
+</template>
+
+<script>
+export default {
+  name: "SuperscriptText",
+  props: {
+    text: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+sup {
+  font-size: 0.75rem;
+  vertical-align: super;
+  line-height: 0;
+  position: relative;
+  top: -0.3rem;
+}
+</style>
