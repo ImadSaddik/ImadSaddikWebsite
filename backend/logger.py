217ca5acdea51de3b247cab21720576dc7c48600
Imad Saddik <simad3647@gmail.com>
2025-11-02 19:32:12 +0100

    Added a StreamHandler to uvicorn logger for console output

--- backend/logger.py
+++ backend/logger.py
@@ -1,7 +1,11 @@
 import logging
+import sys
 
 uvicorn_access = logging.getLogger("uvicorn.access")
 uvicorn_access.disabled = True
 
 logger = logging.getLogger("uvicorn")
 logger.setLevel(logging.getLevelName(logging.DEBUG))
+
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
