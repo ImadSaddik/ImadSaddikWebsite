c06db2e65b3dd85fb0f111aaf95fcf628a429924
Imad Saddik <simad3647@gmail.com>
2025-11-28 20:47:02 +0100

    Replaced vue config with vite config, removed babel config, and updated the pre commit hook

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,6 @@
 import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   ...pluginVue.configs["flat/recommended"],
---
+++ frontend/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    {
+      name: "vite-plugin-md-raw",
+      transform(code, id) {
+        if (id.endsWith(".md")) {
+          return {
+            code: `export default ${JSON.stringify(code)}`,
+            map: null,
+          };
+        }
+      },
+    },
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 8080,
+  },
+});
