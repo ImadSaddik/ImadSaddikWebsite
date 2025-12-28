f5bf5b658d2244f6c18d43bd39b9c1783e7257a5
Imad Saddik <simad3647@gmail.com>
2025-12-28 09:27:34 +0100

    Bundle shiki, vue, and the rest of packages in distinct files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,38 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     vue(),
-    {
-      name: "vite-plugin-md-raw",
-      transform(code, id) {
-        if (id.endsWith(".md")) {
-          return {
-            code: `export default ${JSON.stringify(code)}`,
-            map: null,
-          };
-        }
+    markdownRawPlugin(),
+    visualizer({
+      title: "Bundle visualizer",
+      filename: "./stats.json",
+      template: "raw-data",
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("shiki") || id.includes("@shikijs")) {
+            return "vendor-shiki";
+          }
+
+          if (id.includes("node_modules")) {
+            if (id.includes("vue") || id.includes("vue-router") || id.includes("@vue")) {
+              return "vendor-vue";
+            }
+            return "vendor-libs";
+          }
+        },
       },
     },
-  ],
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -31,3 +47,17 @@ export default defineConfig({
     include: ["tests/unit/**/*.test.{js,ts,jsx,tsx}"],
   },
 });
+
+function markdownRawPlugin() {
+  return {
+    name: "vite-plugin-md-raw",
+    transform(code, id) {
+      if (id.endsWith(".md")) {
+        return {
+          code: `export default ${JSON.stringify(code)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
