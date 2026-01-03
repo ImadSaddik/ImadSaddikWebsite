a1d402981ccfb854f015dade483c84512b52456b
Imad Saddik <simad3647@gmail.com>
2026-01-03 22:34:08 +0100

    Fix tmux pane indexing to work with my new config

--- bash_scripts/run_all_services_tmux.sh
+++ bash_scripts/run_all_services_tmux.sh
@@ -19,15 +19,15 @@ tmux kill-session -t $SESSION_NAME 2>/dev/null
 tmux new-session -d -s $SESSION_NAME -n "Dev"
 
 # Split the window to create a new pane on top for the other services.
-tmux split-window -v -p 50 -t "$SESSION_NAME:0.0"
+tmux split-window -v -p 50 -t "$SESSION_NAME:1.1"
 
 # Select the top pane and split it horizontally.
-tmux select-pane -t "$SESSION_NAME:0.0"
-tmux split-window -h -p 50 -t "$SESSION_NAME:0.0"
+tmux select-pane -t "$SESSION_NAME:1.1"
+tmux split-window -h -p 50 -t "$SESSION_NAME:1.1"
 
 # Start services in their respective panes
-tmux send-keys -t "$SESSION_NAME:0.0" "echo 'Starting Meilisearch...'; cd '$MEILISEARCH_DIR' && $MEILISEARCH_CMD" C-m
-tmux send-keys -t "$SESSION_NAME:0.1" "echo 'Starting Frontend...'; $BASH_ENV_CMD && cd '$FRONTEND_DIR' && $FRONTEND_CMD" C-m
-tmux send-keys -t "$SESSION_NAME:0.2" "echo 'Starting Backend...'; $BASH_ENV_CMD && cd '$BACKEND_DIR' && $BACKEND_CMD" C-m
+tmux send-keys -t "$SESSION_NAME:1.1" "echo 'Starting Meilisearch...'; cd '$MEILISEARCH_DIR' && $MEILISEARCH_CMD" C-m
+tmux send-keys -t "$SESSION_NAME:1.2" "echo 'Starting Frontend...'; $BASH_ENV_CMD && cd '$FRONTEND_DIR' && $FRONTEND_CMD" C-m
+tmux send-keys -t "$SESSION_NAME:1.3" "echo 'Starting Backend...'; $BASH_ENV_CMD && cd '$BACKEND_DIR' && $BACKEND_CMD" C-m
 
 tmux attach-session -t $SESSION_NAME
