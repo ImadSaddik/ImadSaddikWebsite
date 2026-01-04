9cc516e849419d74769af14f306f188d360eb6fd
Imad Saddik <79410781+ImadSaddik@users.noreply.github.com>
2026-01-04 08:47:40 +0100

    Update the stop all services script to also start counting from 1

--- bash_scripts/stop_all_services_tmux.sh
+++ bash_scripts/stop_all_services_tmux.sh
@@ -4,9 +4,9 @@ SESSION_NAME="imad_saddik_personal_website"
 
 if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
   echo "Session found. Stopping services..."
-  tmux send-keys -t "$SESSION_NAME:0.0" C-c
-  tmux send-keys -t "$SESSION_NAME:0.1" C-c
-  tmux send-keys -t "$SESSION_NAME:0.2" C-c
+  tmux send-keys -t "$SESSION_NAME:1.1" C-c
+  tmux send-keys -t "$SESSION_NAME:1.2" C-c
+  tmux send-keys -t "$SESSION_NAME:1.3" C-c
   sleep 1
   tmux kill-session -t "$SESSION_NAME"
   echo "Session '$SESSION_NAME' stopped."
