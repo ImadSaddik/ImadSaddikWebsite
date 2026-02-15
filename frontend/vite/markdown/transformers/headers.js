ef193f4851f0c0b46c12d506d9df73f23b9279aa
Imad Saddik <simad3647@gmail.com>
2026-02-15 08:24:13 +0000

    feat: update the header transformer to handle duplicate headers

--- frontend/vite/markdown/transformers/headers.js
+++ frontend/vite/markdown/transformers/headers.js
@@ -1,13 +1,17 @@
-export function headerTransformer(md) {
+export function headerTransformer(markdownItInstance) {
   const originalHeaderOpen =
-    md.renderer.rules.heading_open ||
+    markdownItInstance.renderer.rules.heading_open ||
     function (tokens, idx, options, env, self) {
       return self.renderToken(tokens, idx, options);
     };
 
-  md.renderer.rules.heading_open = function (tokens, idx, options, env, self) {
+  markdownItInstance.renderer.rules.heading_open = function (tokens, idx, options, env, self) {
     const token = tokens[idx];
     const headerLevel = token.tag.slice(1);
+
+    if (!env.usedHeaderIds) {
+      env.usedHeaderIds = {};
+    }
 
     if (["2", "3", "4"].includes(headerLevel)) {
       const inlineToken = tokens[idx + 1];
@@ -15,8 +19,20 @@ export function headerTransformer(md) {
         .filter((token) => ["text", "code_inline"].includes(token.type))
         .map((token) => token.content)
         .join("");
+      console.log("title:", title);
 
-      const id = title.toLowerCase().replace(/[^\w]+/g, "-");
+      let id = title
+        .toLowerCase()
+        .replace(/[^\w]+/g, "-")
+        .replace(/^-+|-+$/g, "");
+
+      if (hasProperty(env.usedHeaderIds, id)) {
+        env.usedHeaderIds[id]++;
+        id = `${id}-${env.usedHeaderIds[id]}`;
+      } else {
+        env.usedHeaderIds[id] = 0;
+      }
+      console.log("Generated ID:", id);
 
       token.attrSet("id", id);
       token.attrSet("class", "article-body-header");
@@ -25,4 +41,8 @@ export function headerTransformer(md) {
 
     return originalHeaderOpen(tokens, idx, options, env, self);
   };
+}
+
+function hasProperty(object, key) {
+  return Object.prototype.hasOwnProperty.call(object, key);
 }
