0f330b1569055965cc72ac0e382a37244bb95ba1
Imad Saddik <simad3647@gmail.com>
2026-02-16 20:58:57 +0000

    feat: add admonition transformer

--- frontend/src/constants/content.js
+++ frontend/src/constants/content.js
@@ -1,6 +1,8 @@
 export const ARTICLE_READ_THRESHOLD = 0.25;
 export const MILLISECONDS_PER_MINUTE = 60000;
 
+export const ADMONITION_TYPES = ["tip", "info", "warning", "danger"];
+
 export const DEFAULT_ARTICLE_TITLE = "Imad Saddik - Articles";
 
 export const ARTICLE_TYPES = {
