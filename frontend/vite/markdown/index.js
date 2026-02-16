f02a1c430433e80d61284eaa9b788ccff7620914
Imad Saddik <simad3647@gmail.com>
2026-02-16 21:14:12 +0000

    feat: add code and admonition transformers to markdown processing

--- frontend/vite/markdown/index.js
+++ frontend/vite/markdown/index.js
@@ -3,7 +3,17 @@ import { headerTransformer } from "./transformers/headers.js";
 import { linkTransformer } from "./transformers/links.js";
 import { inlineCodeTransformer } from "./transformers/inlineCode.js";
 import { bulletPointTransformer } from "./transformers/bulletPoints.js";
+import { codeTransformer } from "./transformers/code.js";
+import { admonitionTransformer } from "./transformers/admonitions.js";
 
 export function getMarkdownTransformers() {
-  return [youtubeTransformer, headerTransformer, linkTransformer, inlineCodeTransformer, bulletPointTransformer];
+  return [
+    youtubeTransformer,
+    headerTransformer,
+    linkTransformer,
+    inlineCodeTransformer,
+    bulletPointTransformer,
+    codeTransformer,
+    admonitionTransformer,
+  ];
 }
