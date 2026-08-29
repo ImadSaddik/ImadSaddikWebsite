8f4fd5c1e4428efe6a4a2f620a3d50783b8bfb89
Imad Saddik <simad3647@gmail.com>
2026-08-29 11:20:30 +0100

    feat: add HTML and CSS language support to highlighter

--- frontend/src/services/shiki.js
+++ frontend/src/services/shiki.js
@@ -16,6 +16,8 @@ export async function getHighlighter() {
         import("shiki/langs/javascript.mjs"),
         import("shiki/langs/nginx.mjs"),
         import("shiki/langs/json.mjs"),
+        import("shiki/langs/html.mjs"),
+        import("shiki/langs/css.mjs"),
       ],
       engine: createJavaScriptRegexEngine(),
     });
